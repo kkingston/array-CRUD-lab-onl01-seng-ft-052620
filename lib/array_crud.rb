c2eb47bc55c51dfc5d7b87b265e687a71e784c4a
Kayla Kingston <kkingston09@gmail.com>
2020-05-29 22:46:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ['am', 'am', 'am', 'am']
+  array = ['I', 'am', 'a', 'girl']
   array = [2]
 end
 
